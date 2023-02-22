c4ab56faa942f5b40850bdcf0ba456d291bc71e7
Mahmut Canga <cangamahmut@gmail.com>
2023-02-22 11:28:27 +0000

    init

---
+++ src/main/java/com/mahmutcanga/springreststarter/SpringRestStarterApplication.java
@@ -0,0 +1,13 @@
+package com.mahmutcanga.springreststarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahmutcanga/springreststarter/SpringRestStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmutcanga.springreststarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
